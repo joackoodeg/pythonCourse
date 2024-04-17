d5aeefecf4ba62c78b5a5a223510300260471902
joackoodeg <joackoodegiovanni@gmail.com>
2024-04-17 12:01:30 -0300

    ej 12

---
+++ 3/11.py
@@ -0,0 +1,23 @@
+"""
+NO USE HERENCIA PERO PONGO EJ:
+class Animal:
+    def speak(self):
+        pass
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+# Uso
+dog = Dog()
+print(dog.speak())  # Output: Woof!
+
+cat = Cat()
+print(cat.speak())  # Output: Meow!
+
+
+"""
